f3ae0a76b234362728e93208cc2ddfa03250e0b6
jason brown <j.grant.brown@gmail.com>
2016-12-08 14:38:08 +0000

    Done.

--- index.js
+++ index.js
@@ -3,5 +3,15 @@ return document.querySelector(selector);
 }
 
 function nestedTarget(){
-  
+  return document.getElementById('nested').querySelector('div.target');
+}
+
+
+const ranklist=document.getElementById('app').querySelectorAll('ul.ranked-list li')
+
+function increaseRankBy(n){
+  for (let i=0, l= ranklist.length;i<l;i++){
+ return ranklist[i].innerHtml= (i+n).toString()
+
+}
 }
