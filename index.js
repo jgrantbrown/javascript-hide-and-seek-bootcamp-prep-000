b6adbb462685365f6e585bed426571e97334652d
jason brown <j.grant.brown@gmail.com>
2016-12-09 18:19:54 +0000

    Done.

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function nestedTarget(){
 
 
 function increaseRankBy(n){
-  const ranklist=document.getElementById('app').querySelector('ul.ranked-list li');
+  const ranklist=document.getElementById('app').querySelectorAll('ul.ranked-list li');
   for (let i=0,l=ranklist.length;i<l;i++){
   parseInt(ranklist[i]).innerHTML= document.querySelector(ranklist[i] + n).toString();
 }
